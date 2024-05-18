5197255f289ca7f305d2210eb46e3c5fe2da28a4
ktanguy <144002903+ktanguy@users.noreply.github.com>
2024-05-18 15:47:01 +0200

    myinterview test

---
+++ 0-minoperations.py
@@ -0,0 +1,22 @@
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
+
+# Testing the function with example inputs
+if __name__ == "__main__":
+    n = 4
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
